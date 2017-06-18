d4f4c81f37caa77dc7b79434f472b55988c71ee7
ajlopez <webmaster@ajlopez.com>
2017-06-18 11:31:51 -0300

    Create and reject promise

--- test/promise.js
+++ test/promise.js
@@ -13,3 +13,20 @@ exports['create and resolve promise'] = function (test) {
 		test.done();
 	});
 };
+
+exports['create and reject promise'] = function (test) {
+	test.async();
+	
+	var promise = pp.promise(function (resolve, reject) {
+		reject(42);
+	});
+	
+	promise.then(function (value) {
+		test.equal(value, 42);
+		test.fail();
+	}, function (err) {
+		test.equal(err, 42);
+		test.done();
+	});
+};
+
