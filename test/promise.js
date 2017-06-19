330edde67f49ada9233c93487f9991384db0619b
ajlopez <webmaster@ajlopez.com>
2017-06-19 11:02:18 -0300

    Chain promise and reject it

--- test/promise.js
+++ test/promise.js
@@ -92,3 +92,21 @@ exports['chain then that returns a promise'] = function (test) {
 		test.done();
 	});
 };
+
+exports['chain then that returns a promise and reject it'] = function (test) {
+	test.async();
+	
+	var promise = pp.promise(function (resolve, reject) {
+		resolve(41);
+	});
+	
+	promise.then(function (value) {
+		return pp.promise(function (resolve, reject) { reject(value + 1); });
+	}).then(function (value) {
+		test.fail();
+	}, function (err) {
+		test.equal(err, 42);
+		test.done();
+	});
+};
+
